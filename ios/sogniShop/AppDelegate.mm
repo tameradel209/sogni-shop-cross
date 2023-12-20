35fca31759620d6d3445be3db47d05341128e219
tameradel209 <tameradel209@icloud.com>
2023-12-20 15:48:54 -0500

    ios packages work fine

--- ios/sogniShop/AppDelegate.mm
+++ ios/sogniShop/AppDelegate.mm
@@ -1,11 +1,15 @@
 #import "AppDelegate.h"
-
+#import <Firebase.h>
 #import <React/RCTBundleURLProvider.h>
+#import <GoogleMaps/GoogleMaps.h>
+
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [GMSServices provideAPIKey:@"AIzaSyCh972dP0Wjvcw5iSjusFecbD8KkqKxJdA"];
+  [FIRApp configure];
   self.moduleName = @"sogniShop";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
